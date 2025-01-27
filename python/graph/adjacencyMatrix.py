062ac9a671f62bcae54aaa98cd18f9da78479719
iamarifshaikh <arifamanuddinshaikh@gmail.com>
2025-01-28 00:04:23 +0530

    implemented_matrx_graph

---
+++ python/graph/adjacencyMatrix.py
@@ -0,0 +1,19 @@
+class GraphMatrix:
+    def __init__(self,vertices):
+        self.vertices = vertices
+        # Initialize N x N matrix with all values set to 0
+        self.matrix = [[0 for _ in range (vertices)]for _ in range (vertices)]
+
+    def add_edge(self,node1,node2,directed=False):
+        self.matrix[node1][node2] = 1
+        if not directed:
+            self.matrix[node2][node1] = 1  # For undirected graphs
+
+    def display(self):
+        """
+        Step 3: Display the adjacency matrix.
+        Print the matrix row by row.
+        """
+        print("Adjacency Matrix:")
+        for row in self.matrix:
+            print(row)
